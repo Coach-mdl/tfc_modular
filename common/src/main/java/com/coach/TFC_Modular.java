fd924dedf3f949bb59769475c17fd6bf8e68a0f2
Coach <redbulletboy800@hotmail.com>
2025-10-25 00:29:36 +0100

    Began synergizing. Changed some names. Broken.

--- common/src/main/java/com/coach/TFC_Modular.java
+++ common/src/main/java/com/coach/TFC_Modular.java
@@ -13,12 +13,20 @@ public final class TFC_Modular {
     }
 }
 
-//TODO Turn existing overlay modules into synergies.
-//TODO Fix mace and hammer attack speeds.
-//TODO Add Miapi stun to maces.
-//TODO Allow Miapi tools to be placed on item racks.
-//TODO Add TFC damage types to tools.
-//TODO Add Propicks.
-//TODO Add Chisels.
-//TODO Add Saws.
-//TODO Maybe change texture shading to fit TFC.
+//TODO
+// Turn existing overlay modules into synergies.
+// Rename textures for consistency.
+// Fix mace and hammer attack speeds.
+// Apply Miapi stun to maces and smithing hammers.
+// Allow Miapi tools to be placed on item racks.
+// Add Propicks.
+// Add Chisels.
+// Add Saws.
+// add halberd and warhammer modules reminiscent of an old 1.7.10 TFC addon.
+// add Claymore module for fun. Balance it between a longsword and a zweihander.
+// Add TFC damage types to tools.
+// Allow pride skins to be used on grips.
+// Maybe change texture shading to fit TFC.
+// TFC Gems and effects.
+// Look into material generation. Potentially recategorise some of the wood types.
+// New modular workbench model and textures because I want to.
