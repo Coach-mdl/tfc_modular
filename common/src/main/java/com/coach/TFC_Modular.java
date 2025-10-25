f2756e3efa020bd1ac7364f856670c5f8edf1b94
Coach <redbulletboy800@hotmail.com>
2025-10-25 21:25:00 +0100

    More refactorings.

--- common/src/main/java/com/coach/TFC_Modular.java
+++ common/src/main/java/com/coach/TFC_Modular.java
@@ -16,14 +16,14 @@ public final class TFC_Modular {
 //TODO
 // Turn existing overlay modules into synergies.
 // Rename textures for consistency.
-// Fix mace and hammer attack speeds.
 // Apply Miapi stun to maces and smithing hammers.
 // Allow Miapi tools to be placed on item racks.
-// Add Propicks.
+// Add Prospector's picks.
 // Add Chisels.
 // Add Saws.
 // add halberd and warhammer modules reminiscent of an old 1.7.10 TFC addon.
 // add Claymore module for fun. Balance it between a longsword and a zweihander.
+// Figure out why maces have wrong durability.
 // Add TFC damage types to tools.
 // Allow pride skins to be used on grips.
 // Maybe change texture shading to fit TFC.
