897d757a225a67182f2ef5de549ba3dc7f89f604
Coach <redbulletboy800@hotmail.com>
2025-10-27 20:31:34 +0000

    Synergies and skins. Counterweight pommel skin is not triggering the bottomless synergy on tool handles for some reason. Beats me.

--- common/src/main/java/com/coach/TFC_Modular.java
+++ common/src/main/java/com/coach/TFC_Modular.java
@@ -15,18 +15,18 @@ public final class TFC_Modular {
 
 //TODO
 // Turn existing overlay modules into synergies.
-// Rename textures for consistency.
 // Apply Miapi stun to maces and smithing hammers.
-// Allow Miapi tools to be placed on item racks.
 // Add Prospector's picks.
 // Add Chisels.
 // Add Saws.
 // add halberd and warhammer modules reminiscent of an old 1.7.10 TFC addon.
 // add Claymore module for fun. Balance it between a longsword and a zweihander.
+// Epic fight and better combat movesets.
 // Figure out why maces have wrong durability.
+// Allow Miapi tools to be placed on item racks.
 // Add TFC damage types to tools.
-// Allow pride skins to be used on grips.
 // Maybe change texture shading to fit TFC.
 // TFC Gems and effects.
-// Look into material generation. Potentially recategorise some of the wood types.
+// Modular workbench recipe.
 // New modular workbench model and textures because I want to.
+// Look into material generation. Potentially recategorise some of the wood types.
